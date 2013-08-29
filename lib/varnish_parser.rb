4a2cbba0180dd17d7802468fb5900fd8ff01817d
Joshua Bussdieker <jbussdieker@gmail.com>
2013-08-29 18:23:46 -0400

    Start work on racc grammar

--- lib/varnish_parser.rb
+++ lib/varnish_parser.rb
@@ -1,4 +1,5 @@
 require "varnish_parser/version"
+require "varnish_parser/parser"
 
 module VarnishParser
   # Your code goes here...
---
+++ spec/varnish_parser/parser_spec.rb
@@ -0,0 +1,7 @@
+require 'varnish_parser'
+
+describe VarnishParser::Parser do
+  it "should work" do
+    1.should eql(1)
+  end
+end
