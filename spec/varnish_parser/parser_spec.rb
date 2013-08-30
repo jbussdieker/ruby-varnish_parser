bbe8644ba068dd0f3f66c901767637985c6dd344
Joshua Bussdieker <jbussdieker@gmail.com>
2013-08-29 22:41:46 -0400

    Lil more testing

--- spec/varnish_parser/parser_spec.rb
+++ spec/varnish_parser/parser_spec.rb
@@ -3,12 +3,16 @@ require 'yaml'
 
 describe VarnishParser::Parser do
   before do
-    input   = StringIO.new File.read("spec/varnish_parser/test.vcl")
-    tok     = VarnishParser::Tokenizer.new(input)
-    parser  = VarnishParser::Parser.new(tok)
-    @result = parser.parse.result
     @should = YAML.load(File.read("spec/varnish_parser/test.yml"))
   end
 
-  it { @result.should eql(@should) }
+  def test_file(file)
+    input   = StringIO.new File.read(File.join("spec/varnish_parser", file))
+    tok     = VarnishParser::Tokenizer.new(input)
+    parser  = VarnishParser::Parser.new(tok)
+    @result = parser.parse.result
+  end
+
+  it { test_file("test.vcl").should eql(@should) }
+  it { test_file("test2.vcl").should eql(@should) }
 end
